9b0dcd362030bfb13be8ed7f77fa94b8c17712fe
Austin Wood <i@austinwood.me>
2015-06-24 06:45:26 -0600

    Use current_user in the users controller and views when displaying user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,27 @@ class UsersController < ApplicationController
   before_filter :authorize_user
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user      = User.find(params[:id])
     @languages = Language.all
     User.populate_potential_matches(current_user)
   end
 
   def update
-    user = User.find(params[:id])
-    user.languages.delete_all
+    current_user.languages.delete_all
     user_languages = params[:languages]
     user_languages.each do |lang|
       language = Language.find_by(name: lang)
-      user.languages << language
+      current_user.languages << language
     end
-    user.update_attributes(description: params[:description])
-    user.save
+    current_user.update_attributes(description: params[:description])
+    current_user.save
+
+    redirect_to current_user
+  end
+
+  private
 
   def authorize_user
     redirect_to dashboard_path if current_user.id != params[:id].to_i
