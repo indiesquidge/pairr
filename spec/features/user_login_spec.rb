d83367d724cf92b6fdc1e89940c6679d890d15ed
Austin Wood <i@austinwood.me>
2015-06-23 20:08:17 -0600

    First login should redirect to gather more user info, second login should not

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -19,10 +19,35 @@ RSpec.describe "As a new user", type: :feature do
   end
 
   context "the first time I log in" do
-    it "I should be asked about my 'user information'"
+    it "I should be asked about my 'user information'" do
+      Language.create!(name: "Ruby")
+      Language.create!(name: "Java")
+      login_helper
+
+      page.fill_in "user-description", with: "I love code"
+      page.find("input#Ruby").set(true)
+      page.find("input#Java").set(false)
+      page.find_button("Update info", disabled: true).click
+
+      expect(page).to have_content("Ruby")
+      expect(page).to have_content("I love code")
+      expect(page).not_to have_content("Java")
+    end
   end
 
   context "the second time I log in" do
-    it "I should not be asked about my 'user information'"
+    it "I should not be asked about my 'user information'" do
+      Language.create!(name: "Ruby")
+      Language.create!(name: "Java")
+      login_helper
+
+      page.find("input#Ruby").set(true)
+      page.find("input#Java").set(false)
+      page.find_button("Update info", disabled: true).click
+
+      page.click_on "Logout"
+      page.click_on "Login with GitHub"
+      expect(page).to have_content("Welcome")
+    end
   end
 end
