a5f327ebb87888f72392f8f36bcfc17db089f237
Austin Wood <i@austinwood.me>
2015-06-23 20:57:24 -0600

    Create seed file to populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+json = ActiveSupport::JSON.decode(File.read("db/seeds/sample_users.json"))
+
+all_languages = json.first["languages"].keys
+all_languages.each do |language|
+  Language.create!(name: language)
+  puts "generated language #{language}"
+end
+
+json.each do |data|
+  user = User.create!(
+    login: data["login"],
+    avatar_url: data["avatar_url"],
+    description: data["description"],
+  )
+  preferred_languages = data["languages"]
+  preferred_languages.each do |lang, choice|
+    if choice == 1
+      language = Language.find_by(name: lang)
+      Userlanguage.create!(user_id: user.id, language_id: language.id)
+      puts "#{user.login} prefers #{lang}"
+    end
+  end
+end
