859bc96e0c6301a7feb6f0eda9ce0bb69ffc914b
Austin Wood <i@austinwood.me>
2015-06-24 08:08:54 -0600

    Display all approved matches on user's dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,10 @@ class DashboardController < ApplicationController
     Match.pending_matches_first(current_user)
 
     @random_user = User.find(current_user.potential_matches.first)
+
+    approved_matches = Match.approved_matches(current_user)
+    @approved_pairs  = approved_matches.map do |match|
+      User.find(match.user_id)
+    end
   end
 end
