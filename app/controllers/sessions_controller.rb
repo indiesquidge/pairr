728a64b2446eb5ae55f419fb81e2b247c9c8367f
Austin Wood <i@austinwood.me>
2015-06-23 20:04:27 -0600

    If new user, redirect to user edit page to pick langauges and add description

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,13 @@ class SessionsController < ApplicationController
     user = User.find_or_create_from_auth(request.env["omniauth.auth"])
     if user
       session[:user_id] = user.id
-      redirect_to dashboard_path
-      flash[:success] = "Welcome #{user.login}"
+      if user.languages.empty?
+        redirect_to edit_user_path(user)
+      else
+        redirect_to user_path(user)
+        flash[:success] = "Welcome #{user.login}"
+      end
+      user.save
     else
       redirect_to root_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get    "/auth/:provider/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  resources :users, only: [:edit, :update, :show]
+
   get "/dashboard", to: "dashboard#show"
 end
