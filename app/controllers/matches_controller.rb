6181d5138d719748848ef4bd6ad5adce85065c7d
Austin Wood <i@austinwood.me>
2015-06-24 08:08:12 -0600

    Grab potential match, if both users approve each other, change match status to approved

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,6 +6,18 @@ class MatchesController < ApplicationController
   def update
     potential_match_id = params[:id].to_i
     status             = params[:status]
+
+    potential_match = Match.find_by(user_id: potential_match_id)
+    if potential_match
+      if potential_match.status == "pending" && status == "pending"
+        potential_match.update_attributes(status: 3)
+        Match.create(user_id: current_user.id,
+                     potential_match_id: potential_match_id,
+                     status: "approved")
+        matched_user = User.find(potential_match_id)
+        flash[:notice] = "Congrats #{current_user.login}, you and #{matched_user.login} are a good match!"
+      end
+    end
     Match.create(user_id: current_user.id,
                  potential_match_id: potential_match_id,
                  status: status)
